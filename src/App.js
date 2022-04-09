fc40c976bf9c38afe2ce81546444803bd244ce8f
Gabriel Patricio <gabriel.patricio@pbhub.com.br>
2022-04-09 18:26:17 -0300

    feat: Criar tela de Registro e Atualizar para mudanca de telas por Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
-import UserList from './listarUsers/UserList';
+import UserList from './ListarUsers/UserList';
+import RegisterUSer from './Register/RegisterUser';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <UserList />
+    <div className="app">
+      {/* Verificar qual das paginas deve aparecer, que vai depender se 'newUser' eh true ou false */}
+
+      
+            {/* Listar todos os usuarios */}
+      {/* {screen==='list-users' ? (
+        <>
+          <div className="div-userlist">
+            <UserList setScreen={handleScreen}/>
+          </div>
+        </>
+      ):(
+          <div className="div-regiter">
+            <RegisterUSer setScreen={printbatata} />
+          </div>
+      )} */}
+
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="newUser" element={<RegisterUSer />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  // Mudando para o BrowserRouter para maior facilidade de alteracao entre as paginas
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
